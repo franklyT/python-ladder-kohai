ff9444e53d4b62b94b97b6f6133f4db1ed7a1435
franklyt <franklyt@users.noreply.github.com>
2021-01-15 02:05:52 -0500

    added selfDestruct dom cleanup func

--- src/ts/Script/script.ts
+++ src/ts/Script/script.ts
@@ -2,3 +2,7 @@ function enableBootstrapPopovers() {
   // @ts-ignore
   $('[data-toggle="popover"]').popover({ delay: { show: 500, hide: 100 } });
 }
+
+function selfDestruct() {
+  document.currentScript!.remove();
+}
